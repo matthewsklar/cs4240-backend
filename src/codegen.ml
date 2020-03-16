2b4bef1816eea3130883efe215386206fa0ba955
ohadrau <ohadrau@yahoo.com>
2020-03-16 01:57:42 -0400

    Testing: implement to_string for output, enable flattening, test program
    This sets up a simple pipeline that takes the input program in TigerIR
    and converts every single pass to get a final program. It then flattens
    the instruction sequence into a linear program and prints it out. A
    simple test program using a variety of instructions is included in the
    Codegen module, and the result of compiling that program is printed to
    the command-line when the compiler is run. Be on the lookout for __'s,
    which indicate holes for register allocation to fill in.

--- src/codegen.ml
+++ src/codegen.ml
@@ -1,6 +1,13 @@
-module%language M = struct
-  type expr = [ `Print of string ]
-end
+let example_program =
+  let open TigerIR.Ir in
+  [ Assign ("__x", Int 0);
+    ArrayAssign ("__xs", 3, Ident "__x");
+    Assign ("__n", Ident "__x");
+    Label "start";
+    ArrayStore (Ident "__n", "__xs", Ident "__n");
+    Add ("__n", Ident "__n", Int 1);
+    Brgeq ("start", Ident "__n", Int 3);
+    Callr ("__x", "do_stuff", [Ident "__x"; Ident "__n"; Int 3; Int 6; Int 7]) ]
 
 let () =
-  print_endline "Hello"
+  print_endline (Passes.compile example_program)
--- src/passes.ml
+++ src/passes.ml
@@ -5,8 +5,6 @@ type op = [
   | `Ident of string
 ]
 
-type reg = int
-
 let of_op = function
   | Int i -> `Int i
   | Float _ -> failwith "Floats aren't implemented yet"
@@ -31,7 +29,7 @@ let of_ir = function
   | Return x -> `Return (of_op x)
   | Call (x, y) -> `Call (x, List.map of_op y)
   | Callr (x, y, z) -> `Callr (x, y, List.map of_op z)
-  | ArrayStore (x, y, z) -> `ArrayStore (x, y, of_op z)
+  | ArrayStore (x, y, z) -> `ArrayStore (of_op x, y, of_op z)
   | ArrayLoad (x, y, z) -> `ArrayLoad (x, y, of_op z)
   | ArrayAssign (x, y, z) -> `ArrayAssign (x, y, of_op z)
 
@@ -84,7 +82,6 @@ module%language MipsArith = struct
       | `Andi of string * string * int
       | `Or of string * string * string
       | `Ori of string * string * int
-      | `Mfhi of string
       | `Mflo of string
     ];
     del: [
@@ -127,8 +124,6 @@ module%language MipsMem = struct
   include MipsCond
   type instr = {
     add: [
-      | `Lwi of string * int         (* lw dst, off($zero) *)
-      | `Swi of string * int         (* sw src, off($zero) *)
       | `Lw of string * int * string (* lw dst, off(src) *)
       | `Sw of string * int * string (* sw src, off(dst) *)
     ];
@@ -338,3 +333,71 @@ let[@pass MipsCall => MipsLiStack] remove_pseudos =
         `Block [ `Lw (reg, 0, "sp");
                  `Addi ("sp", "sp", 4) ]
   ]
+
+let rec flatten: MipsLiStack.instr list -> MipsFlat.instr list = function
+  | [] -> []
+  | (`Block parts)::rest ->
+    (flatten parts)@(flatten rest)
+  (* Need to use Obj.magic b/c compiler doesn't know instr <> `Block, so can't :> it *)
+  | instr::rest -> (Obj.magic instr)::(flatten rest)
+
+let rec to_string: MipsFlat.instr list -> string = function
+  | [] -> ""
+  | (`Label lbl)::rest ->
+    Printf.sprintf "%s:\n%s" lbl (to_string rest)
+  | (`Add (dst, rx, ry))::rest ->
+    Printf.sprintf "\tadd $%s, $%s, $%s\n%s" dst rx ry (to_string rest)
+  | (`Addi (dst, rx, imm))::rest ->
+    Printf.sprintf "\taddi $%s, $%s, %d\n%s" dst rx imm (to_string rest)
+  | (`Sub (dst, rx, ry))::rest ->
+    Printf.sprintf "\tsub $%s, $%s, $%s\n%s" dst rx ry (to_string rest)
+  | (`Subi (dst, rx, imm))::rest ->
+    Printf.sprintf "\tsubi $%s, $%s, %d\n%s" dst rx imm (to_string rest)
+  | (`Mult (rx, ry))::rest ->
+    Printf.sprintf "\tmult $%s, $%s\n%s" rx ry (to_string rest)
+  | (`Div (rx, ry))::rest ->
+    Printf.sprintf "\tdiv $%s, $%s\n%s" rx ry (to_string rest)
+  | (`Mflo dst)::rest ->
+    Printf.sprintf "\tmflo $%s\n%s" dst (to_string rest)
+  | (`And (dst, rx, ry))::rest ->
+    Printf.sprintf "\tand $%s, $%s, $%s\n%s" dst rx ry (to_string rest)
+  | (`Andi (dst, rx, imm))::rest ->
+    Printf.sprintf "\tandi $%s, $%s, %d\n%s" dst rx imm (to_string rest)
+  | (`Or (dst, rx, ry))::rest ->
+    Printf.sprintf "\tor $%s, $%s, $%s\n%s" dst rx ry (to_string rest)
+  | (`Ori (dst, rx, imm))::rest ->
+    Printf.sprintf "\tori $%s, $%s, %d\n%s" dst rx imm (to_string rest)
+  | (`J lbl)::rest ->
+    Printf.sprintf "\tj %s\n%s" lbl (to_string rest)
+  | (`Jr reg)::rest ->
+    Printf.sprintf "\tjr $%s\n%s" reg (to_string rest)
+  | (`Jal lbl)::rest ->
+    Printf.sprintf "\tjal %s\n%s" lbl (to_string rest)
+  | (`Beq (rx, ry, lbl))::rest ->
+    Printf.sprintf "\tbeq $%s, $%s, %s\n%s" rx ry lbl (to_string rest)
+  | (`Bne (rx, ry, lbl))::rest ->
+    Printf.sprintf "\tbne $%s, $%s, %s\n%s" rx ry lbl (to_string rest)
+  | (`Blez (rx, lbl))::rest ->
+    Printf.sprintf "\tblez $%s, %s\n%s" rx lbl (to_string rest)
+  | (`Bgez (rx, lbl))::rest ->
+    Printf.sprintf "\tbgez $%s, %s\n%s" rx lbl (to_string rest)
+  | (`Bltz (rx, lbl))::rest ->
+    Printf.sprintf "\tbltz $%s, %s\n%s" rx lbl (to_string rest)
+  | (`Bgtz (rx, lbl))::rest ->
+    Printf.sprintf "\tbgtz $%s, %s\n%s" rx lbl (to_string rest)
+  | (`Lw (dst, off, reg))::rest ->
+    Printf.sprintf "\tlw $%s, %d($%s)\n%s" dst off reg (to_string rest)
+  | (`Sw (src, off, reg))::rest ->
+    Printf.sprintf "\tsw $%s, %d($%s)\n%s" src off reg (to_string rest)
+  | (`Lui (dst, imm))::rest ->
+    Printf.sprintf "\tlui $%s, %d\n%s" dst imm (to_string rest)
+
+let compile program =
+  program |> List.map of_ir
+          |> List.map translate_arith
+          |> List.map translate_cond
+          |> List.map translate_array
+          |> List.map translate_call
+          |> List.map remove_pseudos
+          |> flatten
+          |> to_string
