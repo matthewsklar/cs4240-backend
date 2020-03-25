d959fa771b77d14f17b5035adb8a639eecd4e264
ohadrau <ohadrau@yahoo.com>
2020-03-25 19:39:34 -0400

    Move example TigerIR program to .ir file
    This simply removes the ugly hard-coded TigerIR in src/codegen.ml and
    replaces it with an in.ir file that does the same thing. Run the new
    compiler with `dune exec src/codegen.exe -- -i in.ir -o out.s` and
    set the allocator with --allocator={naive,graph}.

--- src/codegen.ml
+++ src/codegen.ml
@@ -1,42 +1,6 @@
 open Lexing
 open TigerIR
 
-let example_program =
-  let open TigerIR.Ir in
-  let main_body =
-    [ Assign ("__x", Int 0);
-      ArrayAssign ("__xs", 3, Ident "__x");
-      Assign ("__n", Ident "__x");
-      Label "start";
-      ArrayStore (Ident "__n", "__xs", Ident "__n");
-      Add ("__n", Ident "__n", Int 1);
-      Brgeq ("start", Ident "__n", Int 3);
-      Callr ("__x", "do_stuff", [Ident "__x"; Ident "__n"; Int 3; Int 6; Int 7]) ] in
-  let main_data = { floatList = []; intList = [
-    Scalar "__x";
-    Array ("__xs", 3);
-    Scalar "__n"
-  ] } in
-  let main = {
-    name = "main";
-    returnType = None;
-    params = [];
-    data = main_data;
-    body = main_body
-  } in
-  let do_stuff = {
-    name = "do_stuff";
-    returnType = Some TyInt;
-    params = [("a", TyInt); ("b", TyInt); ("c", TyInt); ("d", TyInt);
-              ("e", TyInt); ("f", TyInt); ("g", TyInt)];
-    data = { floatList = []; intList = [] };
-    body = [
-      Add ("x", Ident "e", Ident "a");
-      Add ("x", Ident "x", Ident "f");
-      Return (Ident "g")]
-  } in
-  [main; do_stuff]
-
 let string_of_position filename lexbuf =
   let pos = lexbuf.lex_curr_p in
   Printf.sprintf "%s:%d:%d"
