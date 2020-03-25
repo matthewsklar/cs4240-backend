faa8bbc7098d7bd69e9fc326f41d65207e57a5f0
Matthew Sklar <msklar3@gatech.edu>
2020-03-25 19:12:37 -0400

    Codegen/Examples: Added input, output, and allocator command line arguments and examples
    Added the -i (input filename), -o (output filename), and --allocator (allocator type) options
    when executing the backend through the terminal. The input file is required and specifies the
    TigerIR file to be used as input, while the output file, also required, specifies where to
    output the assembly. The allocator is optional, and defaults to a naive allocator. You can
    choose between the inputs "Naive" (naive allocator) or "Graph" (Chaitlin-Briggs Graph Allocator).
    
    The examples directory has example TigerIR files to be used as inputs to the command.

--- src/codegen.ml
+++ src/codegen.ml
@@ -1,3 +1,6 @@
+open Lexing
+open TigerIR
+
 let example_program =
   let open TigerIR.Ir in
   let main_body =
@@ -31,9 +34,56 @@ let example_program =
   } in
   [main; do_stuff]
 
+let string_of_position filename lexbuf =
+  let pos = lexbuf.lex_curr_p in
+  Printf.sprintf "%s:%d:%d"
+    filename
+    pos.pos_lnum
+    (pos.pos_cnum - pos.pos_bol + 1)
+
+let read_file eval filename =
+  let ic = open_in filename in
+  let lexbuf = Lexing.from_channel ic in
+  match Parser.program Lexer.read lexbuf with
+  | exception Lexer.SyntaxError msg ->
+    Printf.fprintf stderr "Syntax error: %s at %s\n"
+      (string_of_position filename lexbuf)
+      msg
+  | exception _ ->
+    Printf.fprintf stderr "Syntax error at %s\n"
+      (string_of_position filename lexbuf)
+  | prog -> eval prog
+
+let eval out_filename ~allocator prog =
+  let out_file = open_out out_filename in
+  let prog' = Isel.compile_program ~allocator prog in
+  output_string out_file prog'
+
 let () =
+  let in_filename = ref ""
+  and out_filename = ref ""
+  and alloc = ref "naive" in
+  let arg_spec = Arg.[
+    "-i", Set_string in_filename, "Input IR file";
+    "-o", Set_string out_filename, "Output IR file";
+    "--allocator", Set_string alloc, "Type of allocator to use";
+  ] in
+  let program_name = Sys.argv.(0) in
+  Arg.parse arg_spec ignore (Printf.sprintf "Usage: %s -i <input_file> -o <output_file> --allocator <allocator>\n" program_name);
+  if !in_filename = "" then begin
+    Printf.eprintf "Error: No input file given!\n";
+    exit 1
+  end;
+  if !out_filename = "" then begin
+    Printf.eprintf "Error: No output file given!\n";
+    exit 1
+  end;
   let allocator fn body =
     let preallocs = Alloc.init_mapping fn body in
-    let (allocs, new_spills) = Alloc.chaitin_briggs fn preallocs body in
+    let (allocs, new_spills) = match !alloc with
+    | "naive" -> Alloc.naive fn preallocs body
+    | "graph" -> Alloc.chaitin_briggs fn preallocs body
+    | _ -> Printf.eprintf "Error: invalid allocator type\n";
+           exit 1 in
     (Alloc.apply_allocations allocs body, new_spills) in
-  print_endline (Isel.compile_program ~allocator example_program)
+  read_file (eval !out_filename ~allocator) !in_filename
