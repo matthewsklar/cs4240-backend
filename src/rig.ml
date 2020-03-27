d2c9ee5dc16f81fd0d959d7c306ea9281b87772e
ohadrau <ohadrau@yahoo.com>
2020-03-26 21:16:40 -0400

    Rig: create edges between currently defined variable and members of out set
    When dealing with dead defs, there's a couple of edge cases. First take the
    following code:
    
    assign a, 0
    assign b, 5
    return a
    
    If we simply perform a normal liveness analysis + graph coloring, we'll have
    no edges between a & b (since b has no live range). As a result, there's a
    chance that a and b are allocated to the same physical register, resulting
    in a program that returns 5 instead of 0. One simple fix we could make here
    is writing all dead defs to $zero (or running deadcode elimination before).
    However, another similar edge case exists that this wouldn't easily account
    for:
    
    assign a, 10
    assign r, 5
    assign r, a
    return r
    
    Since the first assignment to r has no live range, r is not considered live
    until the next assignment. As a result, there's no overlap between the live
    ranges of a & r (when in reality there should be). Once again there's a
    pretty simple fix here: we can just consider both the in-set and out-set of
    a node to be simultaneously live. This is too conservative though, since it
    means that a variable can never be superseded by another variable in the
    same physical register.
    
    The solution that I've found to work best here is to consider the def'd
    variable live in an imaginary program point after the in set and before the
    out set. Then, I create an edge from the currently def'd variable to each
    member of that node's out set. In the above code, this makes r & a mutually
    exclusive since a is still live in the first def to r.

--- src/rig.ml
+++ src/rig.ml
@@ -33,11 +33,20 @@ let build preallocated locals vars sets =
       and other_permutations = permute2 (curr::seen) unseen in
       curr_combos@other_permutations in
 
-  VMap.iter begin fun _node entry ->
+  VMap.iter begin fun node entry ->
+    (* Dead defs don't show up in live ranges, so to deal with that we make an
+       edge from these defs to every var in their node's out set. This should
+       make sure that a dead assign can't overwrite a live variable, but still
+       allow variables to be superseded by another var in the same register. *)
+    VSet.iter begin fun d ->
+      VSet.iter begin fun v ->
+        if not (is_allocated d) && not (is_allocated v) then G.add_edge rig d v
+      end entry.out_set
+    end (defs node);
     (* Don't combine the in_set & out_set into one bigger set, because
-       these are unique program points. If in_set = {A} and out_set = {B},
-       these variables should not be treated as mutually exlcusive during
-       coloring. *)
+      these are unique program points. If in_set = {A} and out_set = {B},
+      these variables should not be treated as mutually exlcusive during
+      coloring. *)
     List.iter begin fun (a, b) ->
       if not (is_allocated a) && not (is_allocated b) then G.add_edge rig a b
     end (permute2 [] (VSet.elements entry.in_set));
