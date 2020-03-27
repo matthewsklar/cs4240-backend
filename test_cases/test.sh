40ba649e11c9c808b83c622f956a172628bf97d8
ohadrau <ohadrau@yahoo.com>
2020-03-27 13:22:57 -0400

    Test cases: use spim -keepstats to collect runtime stats
    This allows us to track dynamic # of instrs, # of loads, etc. for use
    in the performance evalution!

--- test_cases/test.sh
+++ test_cases/test.sh
@@ -6,8 +6,12 @@ function compile() {
 }
 
 function run_test_case() {
-    spim -f "$1/$1-naive.s" <"$1/$2.in" | tail -n +2 >"$1/$2-naive.out"
-    spim -f "$1/$1-graph.s" <"$1/$2.in" | tail -n +2 >"$1/$2-graph.out"
+    # Run tests w/ keepstats
+    spim -keepstats -f "$1/$1-naive.s" <"$1/$2.in" >"$1/$2-naive.raw"
+    spim -keepstats -f "$1/$1-graph.s" <"$1/$2.in" >"$1/$2-graph.raw"
+    # Trim 1 line from front & 2 lines from back
+    cat "$1/$2-naive.raw" | tail -n +2 | head -n -2 >"$1/$2-naive.out"
+    cat "$1/$2-graph.raw" | tail -n +2 | head -n -2 >"$1/$2-graph.out"
     if cmp -s "$1/$2.out" "$1/$2-naive.out" && cmp -s "$1/$2.out" "$1/$2-graph.out"
     then
       echo "Passed test $1-$2"
